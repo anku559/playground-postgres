a0522ae369846ec1d34928861c8b870dbc191003
anku559 <ankitsingh91@gmail.com>
2024-07-06 17:21:20 +0530

    SQL: Adding ' in column and RETURNING claues

--- sample-dbs/movies/statements.sql
+++ sample-dbs/movies/statements.sql
@@ -26,3 +26,26 @@ SELECT
   CONCAT (first_name, ' ', last_name) as "Full Name"
 FROM
   movies.actors;
+
+---
+-- Inserting the data containing the '
+INSERT INTO
+  movies.customers (first_name, last_name, email, age)
+VALUES
+  ('Alex''s', 'Doe', 'alex@mail.com', 21);
+
+---
+-- Inserting the data containing with RETURNING clause
+INSERT INTO
+  movies.customers (first_name, last_name, email, age)
+VALUES
+  ('Alex''s', 'Doe', 'alex@mail.com', 21),
+  ('Bryan''s', 'Doe', 'alex@mail.com', 21) RETURNING *;
+
+INSERT INTO
+  movies.customers (first_name, last_name, email, age)
+VALUES
+  ('Alex''s', 'Doe', 'alex@mail.com', 21),
+  ('Bryan''s', 'Doe', 'alex@mail.com', 21) RETURNING customer_id,
+  email,
+  CONCAT (first_name, ' ', last_name) AS full_name;
